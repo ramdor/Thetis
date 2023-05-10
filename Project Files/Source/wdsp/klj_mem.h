a2cd8aafd747882d0f60230865aecb748675ae89
sjk7 <radiowebmasters@gmail.com>
2023-05-10 08:39:53 +0100

    WDSP dynamic alloc WIP

--- Project Files/Source/wdsp/klj_mem.h
+++ Project Files/Source/wdsp/klj_mem.h
@@ -1,13 +1,21 @@
 // klj_mem.h
 #pragma once
-/*/
+
 #include <stdlib.h>
 #include <string.h>
 #include <Windows.h>
 #include <Memoryapi.h>
-static size_t const ONE_GIG = 1'000'000'000'000;
 
-static inline void* PermanentStorage() {
-    VirtualAlloc(0, ONE_GIG, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+static const size_t KB = 1024;
+static size_t const MB = 1024 * 1024;
+static size_t const GB = 1024 * 1024 * 1024;
+
+typedef struct Storage {
+    size_t capacity;
+    char* mem;
+} STORAGE, *PSTORAGE;
+
+static inline void* PermanentStorage(STORAGE* p) {
+    p->mem
+        = (char*)VirtualAlloc(0, GB, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 }
-/*/
