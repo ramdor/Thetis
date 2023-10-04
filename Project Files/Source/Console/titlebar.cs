4a8685b25f31e710387ac0895ab067d641f0d372
Richie <richie_github@grange-lane.co.uk>
2023-10-04 20:34:19 +0100

    pre-release tag

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -34,7 +34,7 @@ namespace Thetis
 {
     class TitleBar
     {
-        public const string BUILD_NAME = "";
+        public const string BUILD_NAME = "pre-release";
         public const string BUILD_DATE = "(10/04/23)<FW>"; //MW0LGE_21g <FW> gets replaced in BasicTitle (console.cs) with firmware version
 
         public static string GetString()
