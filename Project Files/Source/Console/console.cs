9acb7f3a06f8a2487c130393e038e11224e0c08c
Richie <richie_github@grange-lane.co.uk>
2023-11-17 19:52:15 +0000

    option to prevent rx1+rx2 attenuator link when using diversity

--- Project Files/Source/Console/console.cs
+++ Project Files/Source/Console/console.cs
@@ -12511,7 +12511,7 @@ namespace Thetis
                 {
                     if (!_setFromOtherAttenuator)
                     {
-                        bool bRX1RX2diversity = (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both
+                        bool bRX1RX2diversity = m_bDiversityAttLinkForRX1andRX2 && (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both
                         if (((nRX1ADCinUse == nRX2ADCinUse) || bRX1RX2diversity) && RX2AttenuatorData != rx1_attenuator_data)
                         {
                             _setFromOtherAttenuator = true;
@@ -12529,6 +12529,13 @@ namespace Thetis
 
                 if (oldData != rx1_attenuator_data) AttenuatorDataChangedHandlers?.Invoke(1, oldData, rx1_attenuator_data);
             }
+        }
+
+        private bool m_bDiversityAttLinkForRX1andRX2 = false;
+        public bool DiversityAttLink //[2.10.3.4]MW0LGE used by diversity form
+        {
+            get { return m_bDiversityAttLinkForRX1andRX2; }
+            set { m_bDiversityAttLinkForRX1andRX2 = value; }
         }
 
         private bool rx2_step_att_present = false;
@@ -12678,7 +12685,7 @@ namespace Thetis
                 {
                     if (!_setFromOtherAttenuator)
                     {
-                        bool bRX1RX2diversity = (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both //MW0LGE_[2.9.0.6]
+                        bool bRX1RX2diversity = m_bDiversityAttLinkForRX1andRX2 && (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both //MW0LGE_[2.9.0.6]
                         if (((nRX1ADCinUse == nRX2ADCinUse) || bRX1RX2diversity) && RX1AttenuatorData != rx2_attenuator_data)
                         {
                             _setFromOtherAttenuator = true;
@@ -21344,7 +21351,7 @@ namespace Thetis
 
                 if (!_mox && !_setFromOtherAttenuator)
                 {
-                    bool bRX1RX2diversity = (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both
+                    bool bRX1RX2diversity = m_bDiversityAttLinkForRX1andRX2 && (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both
                     if (((nRX1ADCinUse == nRX2ADCinUse) || bRX1RX2diversity) && RX2PreampMode != rx1_preamp_mode)
                     {
                         _setFromOtherAttenuator = true;
@@ -21500,7 +21507,7 @@ namespace Thetis
 
                 if (!_mox && !_setFromOtherAttenuator)
                 {
-                    bool bRX1RX2diversity = (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both
+                    bool bRX1RX2diversity = m_bDiversityAttLinkForRX1andRX2 && (diversityForm != null && Diversity2 && diversityForm.EXTDIVOutput == 2); // if using diversity, and both rx's are linked, then we need to attenuate both
                     if (((nRX1ADCinUse == nRX2ADCinUse) || bRX1RX2diversity) && RX1PreampMode != rx2_preamp_mode)
                     {
                         _setFromOtherAttenuator = true;
