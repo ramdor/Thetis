23e0f2cdc52d0752b82b0877e80bb9da603a5e72
Richie <richie_github@grange-lane.co.uk>
2024-08-02 12:46:22 +0100

    rotator reworked
    can now view az, ele or both. Need to remove/readd if previously used

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -34,7 +34,7 @@ namespace Thetis
 {
     class TitleBar
     {
-        public const string BUILD_NAME = "dev_8(wip10c)";
+        public const string BUILD_NAME = "dev_8(wip11)";
         public static string GetString(bool bWithFirmware = true)
         {
             string sRevision = "." + Common.GetRevision();
