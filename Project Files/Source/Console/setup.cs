a43a006ff2fb481eb3ebb7d982f757f64784a52e
Richie <richie_github@grange-lane.co.uk>
2024-09-15 12:47:49 +0100

    rc3 + fixed cw edge length of 9ms

--- Project Files/Source/Console/setup.cs
+++ Project Files/Source/Console/setup.cs
@@ -28020,7 +28020,18 @@ namespace Thetis
         private void udCWEdgeLength_ValueChanged(object sender, EventArgs e)
         {
             if (initializing) return;
-            NetworkIO.SetCWEdgeLength((int)udCWEdgeLength.Value);
+
+            // for variable uncomment the two lines below, and comment the fixed DL1YCF and VE3NEA CW Edges
+            // note: grpCWEdgeLength.Visible = false in the form design, and as this is called from ForceAllEvents()
+            // we can make visible here to keep it all together
+            //grpCWEdgeLength.Visible = true;            
+            //NetworkIO.SetCWEdgeLength((int)udCWEdgeLength.Value);
+
+            // for DL1YCF CW Edge
+            NetworkIO.SetCWEdgeLength(9);
+
+            // for VE3NEA CW Edge
+            //NetworkIO.SetCWEdgeLength(15);
         }
 
         private void radDSPNR2TRND_CheckedChanged(object sender, EventArgs e)
