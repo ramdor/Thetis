4e370d7cc82506f69f3258613aac6a043d842903
Richie <richie_github@grange-lane.co.uk>
2024-10-05 16:55:26 +0100

    Update titlebar.cs

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -37,7 +37,7 @@ namespace Thetis
 {
     class TitleBar
     {
-        public const string BUILD_NAME = "rc4h";
+        public const string BUILD_NAME = "rc4i";
         public static string GetString(bool bWithFirmware = true)
         {
             string sRevision = "." + Common.GetRevision();
