71875c1b7499badcc64518bde1824b9445c802a1
Richie <richie_github@grange-lane.co.uk>
2024-10-24 01:31:15 +0100

    single cal test build

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -37,7 +37,7 @@ namespace Thetis
 {
     class TitleBar
     {
-        public const string BUILD_NAME = "dev6g";
+        public const string BUILD_NAME = "dev7";
         public static string GetString(bool bWithFirmware = true)
         {
             string sRevision = "." + Common.GetRevision();
