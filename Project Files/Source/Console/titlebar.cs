90edd186fef1a35413dc40031e282b1d9aee7bb8
Richie <richie_github@grange-lane.co.uk>
2025-04-27 20:24:13 +0100

    dev8c

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -37,7 +37,7 @@ namespace Thetis
 {
     class TitleBar
     {
-        public const string BUILD_NAME = "dev8b";
+        public const string BUILD_NAME = "dev8c";
         public static string GetString(bool bWithFirmware = true)
         {
             string sRevision = "." + Common.GetRevision();
