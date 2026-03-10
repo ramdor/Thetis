f8e1174ca0e3086a97518bd2ef468d519e2fc28e
Richie <richie_github@grange-lane.co.uk>
2026-03-10 19:19:13 +0000

    modified database merge to try an bring in any setting that we now use in console GetState

--- Project Files/Source/Console/titlebar.cs
+++ Project Files/Source/Console/titlebar.cs
@@ -50,7 +50,7 @@ namespace Thetis
 {
     class TitleBar
     {
-        public const string BUILD_NAME = "dev3n";
+        public const string BUILD_NAME = "dev3n2";
         public static string GetString(bool bWithFirmware = true)
         {
             string sRevision = "." + Common.GetRevision();
